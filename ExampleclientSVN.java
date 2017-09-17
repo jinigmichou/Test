e9a64bc69b56f01aa561d201f0726bdf236668b6
Jijimichou <nicolas.pelce@gmail.com>
2017-09-17 15:08:44 +0200

    content of java svn client

---
+++ ExampleclientSVN.java
@@ -0,0 +1,226 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.tmatesoft.svn.core.SVNCommitInfo;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNLogEntry;
+import org.tmatesoft.svn.core.SVNLogEntryPath;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
+import org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory;
+import org.tmatesoft.svn.core.internal.io.fs.FSRepositoryFactory;
+import org.tmatesoft.svn.core.internal.io.svn.SVNRepositoryFactoryImpl;
+import org.tmatesoft.svn.core.io.ISVNEditor;
+import org.tmatesoft.svn.core.io.SVNRepository;
+import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
+import org.tmatesoft.svn.core.wc.SVNWCUtil;
+
+
+public class History {
+    /*
+     * args parameter is used to obtain a repository location URL, a start
+     * revision number, an end revision number, user's account name & password
+     * to authenticate him to the server.
+     */
+    public static void main(String[] args) {
+        /*
+         * Default values:
+         */
+        String url = "";
+        String name = "";
+        String password = "";
+        long startRevision = 0;
+        long endRevision = -1;//HEAD (the latest) revision
+        /*
+         * Initializes the library (it must be done before ever using the
+         * library itself)
+         */
+        setupLibrary();
+
+        if (args != null) {
+            /*
+             * Obtains a repository location URL
+             */
+            url = (args.length >= 1) ? args[0] : url;
+            /*
+             * Obtains the start point of the revisions range
+             */
+            startRevision = (args.length >= 2) ? Long.parseLong(args[1])
+                    : startRevision;
+            /*
+             * Obtains the end point of the revisions range
+             */
+            endRevision = (args.length >= 3) ? Long.parseLong(args[2])
+                    : endRevision;
+            /*
+             * Obtains an account name (will be used to authenticate the user to
+             * the server)
+             */
+            name = (args.length >= 4) ? args[3] : name;
+            /*
+             * Obtains a password
+             */
+            password = (args.length >= 5) ? args[4] : password;
+        }
+
+        SVNRepository repository = null;
+
+        try {
+            /*
+             * Creates an instance of SVNRepository to work with the repository.
+             * All user's requests to the repository are relative to the
+             * repository location used to create this SVNRepository.
+             * SVNURL is a wrapper for URL strings that refer to repository locations.
+             */
+            repository = SVNRepositoryFactory.create(SVNURL.parseURIEncoded(url));
+        } catch (SVNException svne) {
+            /*
+             * Perhaps a malformed URL is the cause of this exception.
+             */
+            System.err
+                    .println("error while creating an SVNRepository for the location '"
+                            + url + "': " + svne.getMessage());
+            System.exit(1);
+        }
+
+
+        ISVNAuthenticationManager authManager = SVNWCUtil.createDefaultAuthenticationManager(name, password);
+        repository.setAuthenticationManager(authManager);
+
+
+
+        /*
+         * Gets the latest revision number of the repository
+         */
+        try {
+            endRevision = repository.getLatestRevision();
+        } catch (SVNException svne) {
+            System.err.println("error while fetching the latest repository revision: " + svne.getMessage());
+            System.exit(1);
+        }
+
+        Collection logEntries = null;
+        try {
+
+            logEntries = repository.log(new String[] {""}, null,
+                    startRevision, endRevision, true, true);
+
+        } catch (SVNException svne) {
+            System.out.println("error while collecting log information for '"
+                    + url + "': " + svne.getMessage());
+            System.exit(1);
+        }
+        for (Iterator entries = logEntries.iterator(); entries.hasNext();) {
+            /*
+             * gets a next SVNLogEntry
+             */
+            SVNLogEntry logEntry = (SVNLogEntry) entries.next();
+            System.out.println("---------------------------------------------");
+            /*
+             * gets the revision number
+             */
+            System.out.println("revision: " + logEntry.getRevision());
+            /*
+             * gets the author of the changes made in that revision
+             */
+            System.out.println("author: " + logEntry.getAuthor());
+            /*
+             * gets the time moment when the changes were committed
+             */
+            System.out.println("date: " + logEntry.getDate());
+            /*
+             * gets the commit log message
+             */
+            System.out.println("log message: " + logEntry.getMessage());
+            /*
+             * displaying all paths that were changed in that revision; cahnged
+             * path information is represented by SVNLogEntryPath.
+             */
+            String logMessage = "log message";
+
+            try {
+                 ISVNEditor editor = repository.getCommitEditor( logMessage , null /*locks*/ , true /*keepLocks*/ , null /*mediator*/ );
+
+                History.copyDir(editor, "C:/svnCommitCode/src","svnurl/src", logEntry.getRevision());
+            } catch (SVNException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+
+            if (logEntry.getChangedPaths().size() > 0) {
+                System.out.println();
+                System.out.println("changed paths:");
+                /*
+                 * keys are changed paths
+                 */
+                Set changedPathsSet = logEntry.getChangedPaths().keySet();
+
+                for (Iterator changedPaths = changedPathsSet.iterator(); changedPaths
+                        .hasNext();) {
+                    /*
+                     * obtains a next SVNLogEntryPath
+                     */
+                    SVNLogEntryPath entryPath = (SVNLogEntryPath) logEntry
+                            .getChangedPaths().get(changedPaths.next());
+                    /*
+                     * SVNLogEntryPath.getPath returns the changed path itself;
+                     * 
+                     * SVNLogEntryPath.getType returns a charecter describing
+                     * how the path was changed ('A' - added, 'D' - deleted or
+                     * 'M' - modified);
+                     * 
+                     * If the path was copied from another one (branched) then
+                     * SVNLogEntryPath.getCopyPath &
+                     * SVNLogEntryPath.getCopyRevision tells where it was copied
+                     * from and what revision the origin path was at.
+                     */
+                    System.out.println(" "
+                            + entryPath.getType()
+                            + " "
+                            + entryPath.getPath()
+                            + ((entryPath.getCopyPath() != null) ? " (from "
+                                    + entryPath.getCopyPath() + " revision "
+                                    + entryPath.getCopyRevision() + ")" : ""));
+                }
+            }
+        }
+    }
+
+    /*
+     * Initializes the library to work with a repository via 
+     * different protocols.
+     */
+    private static void setupLibrary() {
+        /*
+         * For using over http:// and https://
+         */
+        DAVRepositoryFactory.setup();
+        /*
+         * For using over svn:// and svn+xxx://
+         */
+        SVNRepositoryFactoryImpl.setup();
+
+        /*
+         * For using over file:///
+         */
+        FSRepositoryFactory.setup();
+    }
+
+
+    private static SVNCommitInfo copyDir( ISVNEditor editor , String srcDirPath , String dstDirPath , long revision ) throws SVNException {
+                    editor.openRoot( -1 );
+
+                    editor.addDir( dstDirPath , srcDirPath , revision );
+            System.out.println("done--------------------");
+                    //Closes dstDirPath.
+                    editor.closeDir( );
+
+                    //Closes the root directory.
+                   editor.closeDir( );
+
+                   return editor.closeEdit( );
+               }
+
+}
